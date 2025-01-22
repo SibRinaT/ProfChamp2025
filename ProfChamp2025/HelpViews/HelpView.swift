ffbd8c3f19f2ce1394e42c7e7f601df63dbf3bd9
Ainur <sibrinat616@gmail.com>
2025-01-22 08:19:38 +0300

    test

---
+++ ProfChamp2025/HelpViews/HelpView.swift
@@ -0,0 +1,18 @@
+//
+//  HelpView.swift
+//  ProfChamp2025
+//
+//  Created by Ainur on 22.01.2025.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelpView()
+}
